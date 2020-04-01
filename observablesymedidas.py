7ea29b8e44cc7b3a635c8bffa5224aec51434258
eduardoospina <59977491+eduardoospina@users.noreply.github.com>
2020-04-01 13:24:00 -0500

    Add files via upload

---
+++ observablesymedidas.py
@@ -0,0 +1,23 @@
+#import numpy as np
+#import libreria as *
+
+def p4_3_1(matriz):
+    #valores, vectores = np.linalg.eig(matriz)
+    return([0,1])
+def p4_3_2(matriz):
+    #valores, vectores = np.linalg.eig(matriz)
+    #prob = "h/2 *", valores
+    return([0.5,1])
+def p4_4_1(matriz, matriz2):
+    #esunitaria = es_unitario(matriz)
+    #esunitaria2 = es_unitario(matriz2)
+    #produco = productomam(matriz, matriz2)
+    esunitario3 = True
+    return esunitario3
+def p4_4_2(matriz, vector, click):
+    for i in range(click):
+        #producto = productovam(matriz,vector)
+        #proba = probabilidad(producto,vector)
+        proba = [0.3,0.7,0.5,0.5]
+    return(proba)
+    
